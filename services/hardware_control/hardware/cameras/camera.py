616e1abcb0e03309c573d0e298da62d31ded4fda
Will Hutcheson <will.hutcheson@gmail.com>
2022-10-09 10:38:46 -0500

    Add support for free movement and zooming

--- services/hardware_control/hardware/cameras/camera.py
+++ services/hardware_control/hardware/cameras/camera.py
@@ -6,6 +6,7 @@
 
 from datetime import datetime
 import socket
+import time
 
 import requests
 
@@ -54,6 +55,59 @@ def send_inquiry(camera, msg):
     return response
 
 
+def hex_to_int(_bytes):
+    _int = 0
+    for index, byte in enumerate(_bytes[::-1]):
+        _int += byte * (16 ** index)
+    return _int
+
+
+def int_to_hex(_int, min_length=1):
+    _bytes = []
+    power = 0
+    while _int > 0:
+        power += 1
+        remainder = _int % (16 ** power)
+        byte = remainder // (16 ** (power - 1))
+        _bytes.append(byte)
+        _int -= remainder
+    while len(_bytes) < min_length:
+        _bytes += b"\x00"
+    return bytes(_bytes)[::-1]
+
+
+def get_position(camera):
+    msg = b"\x81\x09\x06\x12\xFF"
+    data = send_inquiry(camera, msg)
+    pan = hex_to_int(data[1:5])
+    tilt = hex_to_int(data[5:])
+    return (pan, tilt)
+
+
+def set_position(camera, pan, tilt, speed=10):
+    msg = b"\x81\x01\x06\x02"
+    # speed 1-20 tilt, 1-24 pan
+    msg += bytes((speed, speed))
+    msg += int_to_hex(pan, min_length=4)
+    msg += int_to_hex(tilt, min_length=4)
+    msg += b"\xFF"
+    send_command(camera, msg)
+
+
+def get_zoom(camera):
+    msg = b"\x81\x09\x04\x47\xFF"
+    data = send_inquiry(camera, msg)
+    zoom = hex_to_int(data[1:5])
+    return zoom
+
+
+def set_zoom(camera, zoom):
+    msg = b"\x81\x01\x04\x47"
+    msg += int_to_hex(zoom, min_length=4)
+    msg += b"\xFF"
+    send_command(camera, msg)
+
+
 def recall(camera, preset):
     msg = b"\x81\x01\x04\x3F\x02"
     msg += bytes((preset,))
@@ -73,6 +127,62 @@ def clear(camera, preset):
     msg += bytes((preset,))
     msg += b"\xFF"
     send_command(camera, msg)
+
+
+def stop(camera, speed=10):
+    msg = b"\x81\x01\x06\x01"
+    msg += bytes((speed, speed))
+    msg += b"\x03\x03\xFF"
+    send_command(camera, msg)
+
+
+def home(camera):
+    msg = b"\x81\x01\x06\x04\xFF"
+    send_command(camera, msg)
+
+
+def reset(camera):
+    msg = b"\x81\x01\x06\x05\xFF"
+    send_command(camera, msg)
+
+
+def _move_direction(camera, direction, allow_time=0.3, speed=10):
+    msg = b"\x81\x01\x06\x01"
+    msg += bytes((speed, speed))
+    msg += direction
+    msg += b"\xFF"
+    send_command(camera, msg)
+    if (allow_time):
+        time.sleep(allow_time)
+    stop(camera, speed)
+
+
+def up(camera, allow_time=0.3, speed=10):
+    _move_direction(camera, b"\x03\x01", allow_time=allow_time, speed=speed)
+
+
+def down(camera, allow_time=0.3, speed=10):
+    _move_direction(camera, b"\x03\x02", allow_time=allow_time, speed=speed)
+
+
+def left(camera, allow_time=0.3, speed=10):
+    _move_direction(camera, b"\x01\x03", allow_time=allow_time, speed=speed)
+
+
+def right(camera, allow_time=0.3, speed=10):
+    _move_direction(camera, b"\x02\x03", allow_time=allow_time, speed=speed)
+
+
+def zoom_in(camera, amount=500):
+    zoom = get_zoom(camera)
+    new_zoom = max(0, min(16384, zoom + amount))
+    set_zoom(camera, new_zoom)
+
+
+def zoom_out(camera, amount=500):
+    zoom = get_zoom(camera)
+    new_zoom = max(0, min(16384, zoom - amount))
+    set_zoom(camera, new_zoom)
 
 
 def get_still(camera, path=None):
